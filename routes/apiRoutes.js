d5ba5a368b74f7fbc87ea14eab23ee14d5184af5
Susye Weng-Reeder <weng-reeder@hotmail.com>
2019-01-11 12:45:31 -0800

    apiRoutes htmlRoutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,12 +2,27 @@ var express = require("express");
 var router = express.Router();
 
 
-router.get("/burgers", function(req, res) {
-    res.render("index");
-})
 
-router.post("/customers", function (req, res)) {
-    res.render("index");
-}
+router.get("/api/burgers", function (req, res) {
+    db.Burger.findAll({}).then(function (dbburgers) {
+        res.json(dbBurgers);
+    });
+});
+
+// Create a new Burger
+router.post("/api/burgers", function (req, res) {
+    db.Burger.create(req.body).then(function (dbBurger) {
+        res.json(dbBurger);
+    });
+});
+
+// Delete a Burger by id
+router.delete("/api/burgers/:id", function (req, res) {
+    db.Burger.destroy({ where: { id: req.params.id } }).then(function (dbBurger) {
+        res.json(dbBurger);
+    });
+});
+
+
 
 module.exports = router;
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,8 +2,28 @@ var express = require("express");
 var router = express.Router();
 
 
-router.get("/", function(req, res) {
-    res.render("index");
-})
+router.get("/", function (req, res) {
+    db.Burger.findAll({}).then(function (dbBurgers) {
+        res.render("index", {
+            burgers: dbBurgers
+        });
+    });
+});
+
+router.get("/burger/:id", function (req, res) {
+    db.Buger.findOne({
+        where: { id: req.params.id }
+    }).then(function (dbBurger) {
+        res.render("burger", {
+            burger: dbBurger
+        });
+    });
+});
+
+
+router.get("*", function (req, res) {
+    res.render("404");
+});
+
 
 module.exports = router;
